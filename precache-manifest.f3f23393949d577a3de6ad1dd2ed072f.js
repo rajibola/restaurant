6e8f31c14e2ab976b4b35ea33a5835d0f09fab46
ridwan <rajibola1997@gmail.com>
2019-12-25 15:59:44 -0800

    Updates

---
+++ precache-manifest.f3f23393949d577a3de6ad1dd2ed072f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2ba5f876607dd24dc886ef9b84bafe",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "8eb6556cbc8eb3cfc235",
+    "url": "/restaurant/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b45432c47152066fe02b",
+    "url": "/restaurant/static/css/main.ec72dc86.chunk.css"
+  },
+  {
+    "revision": "8eb6556cbc8eb3cfc235",
+    "url": "/restaurant/static/js/2.cee49b1f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/restaurant/static/js/2.cee49b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b45432c47152066fe02b",
+    "url": "/restaurant/static/js/main.12831e77.chunk.js"
+  },
+  {
+    "revision": "608d830621a748ac6760",
+    "url": "/restaurant/static/js/runtime-main.2c61200c.js"
+  }
+]);
